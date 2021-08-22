d898412c3c1d7aadc65b9facaa2f2ecfe379d28f
Your Name <nguenviktoria@gmail.com>
2021-08-22 03:02:43 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{1,10}/gi);
+    let str = arr.join(',');
+    let nothing = str.replace(/00/gi, '');
+    let drop = nothing.replace(/10/gi, '.');
+    let line = drop.replace(/11/gi, '-');
+    let star = line.replace(/\*\*\*\*\*\*\*\*\*\*/gi, ' ');
+    let newString = line.split(',');
+    let array = [];
+        for (let element in newString) {
+            for (let prop in MORSE_TABLE) {
+                if (newString[element] === prop) {
+                    array.push(MORSE_TABLE[prop]);
+                }
+            }
+        }
+        let newArray = array.join('');
+    return newArray;
 }
 
 module.exports = {
